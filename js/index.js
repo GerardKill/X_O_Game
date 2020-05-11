4432fec2dddc665f49aaa47385d44f420b7eb859
GerardKill <vladhulkjoker96@gmail.com>
2020-05-12 01:00:46 +0300

    create add function

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 "use strict"
 
-	cell_0_0.onclick = function(){
-		let element = document.getElementById('cross_0_0');
+function add(a, b, c){
+	a.onclick = function(){
+		let element = document.getElementById(b);
 		let color = window.getComputedStyle(element).opacity;
-		let element1 = document.getElementById('null_0_0');
+		let element1 = document.getElementById(c);
 		let color1 = window.getComputedStyle(element1).opacity;
 		if((color == 0) && (color1 == 0)){
-			document.getElementById('cross_0_0').style.opacity = "1";
+			document.getElementById(b).style.opacity = "1";
 		}
 	}
+}
+
+
+
+	add(cell_0_0, "cross_0_0", "null_0_0");
+	add(cell_0_1, "cross_0_1", "null_0_1");
