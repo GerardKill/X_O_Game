bd0ee7d6abc0f3c81f0415065b1730dd7f2b57e4
GerardKill <vladhulkjoker96@gmail.com>
2020-05-17 14:59:56 +0300

    X and O style, and little click fix

--- js/index.js
+++ js/index.js
@@ -16,3 +16,10 @@ function add(a, b, c){
 
 	add(cell_0_0, "cross_0_0", "null_0_0");
 	add(cell_0_1, "cross_0_1", "null_0_1");
+	add(cell_0_2, "cross_0_2", "null_0_2");
+	add(cell_1_0, "cross_1_0", "null_1_0");
+	add(cell_1_1, "cross_1_1", "null_1_1");
+	add(cell_1_2, "cross_1_2", "null_1_2");
+	add(cell_2_0, "cross_2_0", "null_2_0");
+	add(cell_2_1, "cross_2_1", "null_2_1");
+	add(cell_2_2, "cross_2_2", "null_2_2");
